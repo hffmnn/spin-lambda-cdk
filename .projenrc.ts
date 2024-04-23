57bb34d62ff0e8e5352dd17b682154db9c82fcc0
Christian Hoffmann <ch.does.things@gmail.com>
2024-04-23 21:20:41 +0200

    add dependencies

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,7 +3,7 @@ import { TrailingComma } from "projen/lib/javascript";
 const project = new awscdk.AwsCdkConstructLibrary({
   author: "Christian Hoffmann",
   authorAddress: "ch.does.things@gmail.com",
-  cdkVersion: "2.1.0",
+  cdkVersion: "2.133.0",
   defaultReleaseBranch: "main",
   jsiiVersion: "~5.0.0",
   name: "spin-lambda-cdk",
@@ -18,10 +18,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
       trailingComma: TrailingComma.ALL,
     },
   },
+  gitignore: ["cdk.out", "cdk.context.json"],
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: ["nodejs-file-downloader", "ora@5", "toml"],
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: ["aws-cdk", "ts-node"],
   // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
